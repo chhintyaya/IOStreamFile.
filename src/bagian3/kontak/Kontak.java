83243adc8e227c05b259fcf0a610dc999747d60d
Chintya Mahfuzah <chhintyaya@gmail.com>
2026-06-25 21:22:58 +0800

    Selesai Bagian 3: Multi-Class

--- src/bagian3/kontak/Kontak.java
+++ src/bagian3/kontak/Kontak.java
@@ -1,32 +1,33 @@
 package bagian3.kontak;
 
 public class Kontak {
-    
-    private String nama; 
+    private String nama;
     private String nomor;
+    private String email;
 
-   
-    public Kontak(String nama, String nomor) { 
+    public Kontak(String nama, String nomor, String email) {
         this.nama = nama;
-        this.nomor = nomor; 
+        this.nomor = nomor;
+        this.email = email;
     }
 
-   
-    public String getNama() { 
+    public String getNama() {
         return nama;
     }
 
-    public String getNomor() { 
-        return nomor; 
+    public String getNomor() {
+        return nomor;
     }
 
-  
-    public String keBaris() { 
-        return nama + ";" + nomor; 
+    public String getEmail() {
+        return email;
     }
 
-    
-    public String info() { 
-        return nama + " " + nomor; 
+    public String keBaris() {
+        return nama + ";" + nomor + ";" + email;
+    }
+
+    public String info() {
+        return nama + " " + nomor + " " + email;
     }
 }
